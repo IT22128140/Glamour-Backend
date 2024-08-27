71aa6d1cb7ece6ca6033871e8d6a55e2f47a6948
IT22128140 <it22128140@my.sliit.lk>
2024-08-27 22:35:46 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,3 @@
+export const PORT = 3000;
+
+export const MONGO_URI = "mongodb+srv://Admin:587bavQG9pMdjFnn@glamour.dfopn.mongodb.net/GlamourDB?retryWrites=true&w=majority&appName=Glamour"
---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import { PORT, MONGO_URI } from "./config.js";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("Hello World!");
+});
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
