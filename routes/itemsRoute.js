42d1fe7b8a01eb1622d11fee9ba5d967cf3abc9b
IT22128140 <it22128140@my.sliit.lk>
2024-09-10 21:20:01 +0530

    Refactor routes and add cusItemsRoute.js

--- routes/itemsRoute.js
+++ routes/itemsRoute.js
@@ -20,13 +20,16 @@ router.post("/", async (request, response) => {
     if (
       !request.body.productId ||
       !request.body.name ||
+      !request.body.description ||
       !request.body.minprice ||
       !request.body.maxprice ||
       !request.body.salesdifference ||
       !request.body.priceincrease ||
       !request.body.category ||
-      !request.body.description ||
       !request.body.image ||
+      !request.body.trending ||
+      !request.body.stock ||
+      !request.body.colors ||
       !request.body.sizes
     ) {
       return response.status(400).send({
@@ -37,15 +40,16 @@ router.post("/", async (request, response) => {
       productId: request.body.productId,
       name: request.body.name,
       description: request.body.description,
-      minprice: request.body.price,
+      minprice: request.body.minprice,
       maxprice: request.body.maxprice,
       salesdifference: request.body.salesdifference,
       priceincrease: request.body.priceincrease,
       category: request.body.category,
       image: request.body.image,
-      colors: request.body.colors,
-      sizes: request.body.sizes,
       trending: request.body.trending,
+      stock: request.body.stock,
+      colors: request.body.colors,
+      sizes: request.body.sizes
     };
 
     const item = await Item.create(newItem);
